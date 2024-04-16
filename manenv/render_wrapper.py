2a038c576a7e4ed5214c7c3f4552b04280745b40
TheRealAnonymous008 <joaquinantoniodelarosa@gmail.com>
2024-04-16 10:00:48 +0800

    basic render

--- manenv/render_wrapper.py
+++ manenv/render_wrapper.py
@@ -4,19 +4,26 @@ from typing import Tuple
 
 from manenv.world import World
 
-DISPLAY_WIDTH = 800
-DISPLAY_HEIGHT = 600
-
 class RenderWrapper: 
     """
     Wraps arond the world for rendering and UI related things. 
     """
-    def __init__(self, world : World): 
-        self.world = world 
+    def __init__(self, world : World, display_dims : Tuple = (1000, 600), cell_dims : Tuple = (100, 100)): 
+        """
+        world - the world object to be rendered
+
+        display_dims - the display width and height of the screen
+
+        visible cells - the number of visible cells on each axis. 
+        """
+        self.world : World = world 
+        self.display_dims : Tuple = display_dims
+        self.cell_dims : Tuple = cell_dims
+        self._visible_cells : Tuple = (int(self.display_dims[0] / self.cell_dims[0]), int(self.display_dims[1] / self.cell_dims[1]))
 
     def render(self):
         pg.init()
-        pg.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+        surface = pg.display.set_mode(self.display_dims)
 
         self.running = True 
         while self.running :
@@ -25,6 +32,23 @@ class RenderWrapper:
                     self.running = False 
                 if event.type == pg.KEYDOWN: 
                     pass 
+            
+            surface.fill(pg.Color(255, 255, 255))
+            # Draw cell bgs
+            for i in range(0, self._visible_cells[0]):
+                for j in range(0, self._visible_cells[1]):
+                    pg.draw.rect(
+                        surface=surface, 
+                        color= pg.Color(0, 0, 0), 
+                        rect = pg.Rect(
+                            i * self.cell_dims[0], 
+                            j * self.cell_dims[1], 
+                            self.cell_dims[0] * 0.95, 
+                            self.cell_dims[1] * 0.95
+                        )
+                    )
+
+            pg.display.flip()
 
         
         pg.quit()
--- manenv/world.py
+++ manenv/world.py
@@ -1,16 +1,32 @@
 import numpy as np 
 import pygame as pg 
-from typing import Tuple
+from typing import Tuple, Iterable
+
+class WorldCell: 
+    """
+    The basic unit within the factory. 
+    """
+    def __init__(self):
+        pass 
+
 
 class World: 
     """
     Contains information about the smart factory environment 
     """
     def __init__(self, shape : Tuple): 
-        self.shape = shape 
+        """
+        shape - the dimensions of the environment in (width, height) format 
+        """
+        self.shape : Tuple = shape 
+        self.map : list[list[WorldCell]] = [[WorldCell() for _ in range(shape[1])] for _ in range(shape[0])]
 
     def _width(self):
         return self.shape[0]
     
     def _height(self):
         return self.shape[1]
+    
+    def get_cell(self, x : int, y : int) -> WorldCell:
+        return self.map[x][y]
+
