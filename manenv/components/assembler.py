ebd0e587585f8ca70e72034ae8352e8b956c08c3
JoaquinDelaRosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2024-05-22 19:22:18 +0800

    feat: assemblers can now take demand as input

--- manenv/components/assembler.py
+++ manenv/components/assembler.py
@@ -4,7 +4,7 @@ from manenv.core.effector import Effector
 from manenv.core.product import Product
 from manenv.utils.product_utils import *
 from manenv.utils.vector import *
-
+from manenv.core.demand import Order
 
 class Assembler(FactoryComponent):
     """
@@ -13,12 +13,18 @@ class Assembler(FactoryComponent):
     `workspace_size`: the size of the workspace supported by this product
 
     `effectors`: the list of effectors that can be used by this assembler unit. 
+
+    `staging_size`: the number of products that can be in the staging area at most
     """
-    def __init__(self, workspace_size : Vector, effectors : list[Effector] = []):
+    def __init__(self, 
+                 workspace_size : Vector, 
+                 effectors : list[Effector] = [],
+                 staging_size : int = -1):
         super().__init__(AssetPath.ASSEMBLER)
         
         self._workspace_size = workspace_size
         self._effectors = effectors
+        self._staging_size = staging_size
 
         self.reset()
 
@@ -36,12 +42,23 @@ class Assembler(FactoryComponent):
             eff._postupdate()
 
         self.update_masks()
+        self.update_staging()
 
     def reset(self):
         self._workspace = np.zeros(self._workspace_size, dtype=int)
         self._product_mask = np.zeros(self._workspace_size, dtype = int)
         self._product_list : dict[int, Product] = {}
-        self._product_outputs = []
+
+        # The staging area is where we first push our products.
+        # We then push it onto the product outputs array to be delivered outside
+        # The assembler. This way, we can match orders in the queue with staged products
+        self._staging_area : list[Product]= []
+        self._product_outputs : list[Product] = []
+
+        self._job_queue : list[Order] = []
+
+    def add_order_to_queue(self, order : Order):
+        self._job_queue.append(order)
 
     def get_product_inventory(self) -> list[Product]:
         return self._cell._products
@@ -106,10 +123,41 @@ class Assembler(FactoryComponent):
         return product
     
     def release_product_in_workspace(self, position: Vector) -> Product:
-        product = self._pop_from_workspace(position)
+        product = self.get_product_in_workspace(position)
         if product == None: 
             return 
+        if len(self._staging_area) >= self._staging_size and self._staging_size > 0: 
+            return 
         
+        self._pop_from_workspace(position)
         product._is_dirty = True 
-        self._product_outputs.append(product)
+        
+        self._staging_area.append(product)
         return product
+    
+    def update_staging(self):
+        for i in range(len(self._staging_area)):
+            p = self._staging_area[i] 
+            idx = self._assess_product_with_queue(p)
+            self._product_outputs.append(p)
+            
+            if idx < 0:
+                continue 
+            
+            self._job_queue.pop(idx)
+            self._staging_area.pop(i)
+
+    def _assess_product_with_queue(self, product : Product) -> int:
+        # TODO: Implement this 
+        # It is recommended that any function for this should take into account
+        # The product's match to a specified order on the list
+        # The current status of product inventory
+        # The current status of the staging area (is it being filled)
+
+        """
+        This determines if the product can be matched to a job in the queue.
+        """
+        if len(self._job_queue) == 0:
+            return -1
+        
+        return 0
