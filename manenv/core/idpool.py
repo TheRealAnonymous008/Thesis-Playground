4d95fd1d65de2201c0b1654d13ddf02cb9eff76e
joaquin <62501229+TheRealAnonymous008@users.noreply.github.com>
2024-06-16 07:59:58 +0800

    clean commit

--- manenv/core/idpool.py
+++ manenv/core/idpool.py
@@ -1,17 +1,24 @@
 import random
+import threading
 
-
-class IDPool: 
+class IDPool:
     """
     A helper class to assign unique IDs to anything
     """
-    _IDs : set = set()
+    _IDs: set = set()
+    _counter: int = 0
+    _lock = threading.Lock()  # To make the ID generation thread-safe
 
-    def get() -> int:
-        _id = random.getrandbits(31)
-        IDPool._IDs.add(_id)
+    @classmethod
+    def get(cls) -> int:
+        with cls._lock:
+            _id = cls._counter
+            cls._counter += 1
+            cls._IDs.add(_id)
         return _id 
-    
-    def pop(_id : int): 
-        if _id in IDPool._IDs: 
-            IDPool._IDs.remove(_id)
+
+    @classmethod
+    def pop(cls, _id: int): 
+        with cls._lock:
+            if _id in cls._IDs:
+                cls._IDs.remove(_id)
