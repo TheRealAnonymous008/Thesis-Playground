eaa58f8732b1d450dcc1a898ec109850a9511047
joaquin <62501229+TheRealAnonymous008@users.noreply.github.com>
2024-09-06 11:56:36 +0800

    feat: agents exist in the world

--- v2/core/agent.py
+++ v2/core/agent.py
@@ -1,3 +1,43 @@
+from __future__ import annotations
+
+import numpy as np
+from enum import Enum
+
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    EAST = 3
+    WEST = 4
+
 class Agent:
     def __init__(self):
-        pass 
+        """
+        Initializes a simple agent 
+        """
+        self._position : tuple[int, int] = (0, 0)
+
+        self._current_action = None
+
+    def set_position(self, position : tuple[int, int]):
+        """
+        Set the agent's position to `position`
+        """
+        self._position = position
+
+    def move(self, dir : Direction | int):
+        """
+        Moves an agent along a specified direction. 
+        The direction is either a Direction instance or an integer associated with a Direction value.
+        """
+        if type(dir) is Direction: 
+            val = dir.value
+        else: 
+            val = dir 
+
+        match(val):
+            case Direction.NORTH.value: pass 
+            case Direction.SOUTH.value: pass 
+            case Direction.EAST.value: pass 
+            case Direction.WEST.value: pass
+            case _: 
+                raise Exception(f"Invalid direction specified {val}")
--- v2/core/world.py
+++ v2/core/world.py
@@ -1,3 +1,56 @@
+from __future__ import annotations
+
+import numpy as np
+from .agent import Agent
+
 class World: 
-    def __init__(self):
-        pass 
+    def __init__(self, 
+                 dims : tuple[int, int]
+        ):
+        """
+        `dims`: Dimensions of the world (x, y) form.
+        """
+        if dims[0] <= 0 or dims[1] <= 0:
+            raise Exception(f"Incorrect dimensions {dims}")
+        
+        self._dims : tuple[int, int] = dims
+        self._agents : set[Agent] = set()
+        self._time_step = 0
+
+    def reset(self):
+        """
+        Reset the environment
+        """
+        self._agents.clear()
+        self._time_step = 0 
+
+    def update(self):
+        """
+        Update the environment 
+        """
+
+        self._time_step += 1
+
+    def add_agent(self, agent : Agent):
+        """
+        Adds an `agent` to the environment
+        """
+        self._agents.add(agent)
+
+    def remove_agent(self, agent : Agent):
+        """
+        Removes `agent` from the environment. Assumes `agent` is in the environment
+        """
+        self._agents.remove(agent)
+
+
+def initialize_positions_randomly(world : World, swarm : list[Agent]):
+    positions = set()
+    while len(positions) < len(swarm):
+        x, y = np.random.randint(0, world._dims[0]), np.random.randint(0, world._dims[1])
+        positions.add((x, y))
+
+    for agent, pos in zip(swarm, positions):
+        agent.set_position(pos)
+
+    return swarm
