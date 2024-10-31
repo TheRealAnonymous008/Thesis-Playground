fix: potential broadcast error in idqn

v2/models/idqn.py
@@ -90,15 +90,16 @@ class IDQN(BaseModel):
         states, actions, rewards, next_states, dones = experiences
 
         agents = self.env.agents
+        average_loss = 0
 
         for i, agent in enumerate(agents) : 
             # Compute Q(s_t, a)
             state_action_values = self.policy_net.forward(agent, states)
-            state_action_values = state_action_values.gather(1, actions[agent])
+            state_action_values = state_action_values.gather(1, actions[agent]).squeeze(1)
 
             # Compute V(s_{t+1}) using the target network
             with torch.no_grad():
-                next_state_values = self.target_net.forward(agent, next_states).max(1)[0].unsqueeze(1)
+                next_state_values = self.target_net.forward(agent, next_states).max(1)[0]
                 expected_state_action_values = (next_state_values * self.gamma * (1 - dones[i])) + rewards[i]
 
             # Compute loss
@@ -107,7 +108,10 @@ class IDQN(BaseModel):
             # Optimize the model
             self.optimizer.zero_grad()
             loss.backward
+            average_loss += loss.item()
             self.optimizer.step()
+
+        print(f"Average loss {average_loss / len(agents)}")
 
         # Soft update the target network
         self.soft_update()

v2/solution/policy_net.py
@@ -58,6 +58,7 @@ class PolicyNet(nn.Module):
         return x
 
 import torch
+import numpy as np
 
 
 def feature_extractor(obs : dict) -> TensorDict:
@@ -75,7 +76,7 @@ def feature_extractor(obs : dict) -> TensorDict:
         energy = agent_obs['Energy']
         belief = agent_obs['Belief']
 
-        vision_tensor = torch.tensor([vision_grid], dtype=torch.float32)
+        vision_tensor = torch.tensor(vision_grid, dtype=torch.float32).unsqueeze(0)
 
         # Convert energy and belief to tensors (they could be single values or vectors)
         energy_tensor = torch.tensor([energy], dtype=torch.float32)