cb8913e7bedc3de49faa648601b7d922bb30c46a
joaquin <62501229+TheRealAnonymous008@users.noreply.github.com>
2025-06-11 17:46:59 +0800

    .

--- v3/models/hypernet.py
+++ v3/models/hypernet.py
@@ -27,108 +27,6 @@ class LatentEncoder(nn.Module):
         sigma = self.std_net(inputs) + 1e-8
 
         return mu, sigma
-
-class SACLatentDecoder(nn.Module): 
-    def __init__(self, config: ParameterSettings):
-        super().__init__()
-        self.config = config
-        self.enable_spectral_norm = True
-        self.dropout_rate = -1
-
-        # Policy
-        self.p_net_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights * config.d_action], enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        self.p_net_biases = make_net([config.d_het_latent, 256, 256, 256, config.d_action], enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate = self.dropout_rate)
-
-        # Critic
-        self.crit_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights], enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        self.crit_biases = make_net([config.d_het_latent, 256, 256, 256, 1] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-
-        # Belief
-        self.b_net_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights * config.d_beliefs] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        self.b_net_biases = make_net([config.d_het_latent, 256, 256, 256, config.d_beliefs] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-
-        # Encoder
-        self.e_net_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights * config.d_comm_state] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        self.e_net_biases = make_net([config.d_het_latent, 256, 256, 256, config.d_comm_state] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-
-        # Filter
-        self.f_net_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights * config.d_message] , enable_spectral_norm= self.enable_spectral_norm , last_activation = False, dropout_rate= self.dropout_rate)
-        self.f_net_biases = make_net([config.d_het_latent, 256, 256, 256, config.d_message] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-
-        # Decoder
-        self.d_net_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights * config.d_comm_state] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        self.d_net_biases = make_net([config.d_het_latent, 256, 256, 256, config.d_comm_state] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-
-        # Update
-        self.u_mean_net_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights * config.d_relation] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        self.u_mean_net_biases = make_net([config.d_het_latent, 256, 256, 256, config.d_relation] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        
-        self.u_bias_net_weights = make_net([config.d_het_latent, 256, 256, 256, config.d_het_weights * config.d_relation] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-        self.u_bias_net_biases = make_net([config.d_het_latent, 256, 256, 256, config.d_relation] , enable_spectral_norm= self.enable_spectral_norm, last_activation = False, dropout_rate= self.dropout_rate)
-
-    def to(self, device):
-        # Policy networks
-        self.p_net_weights.to(device)
-        self.p_net_biases.to(device)
-
-        # Critic networks
-        self.crit_weights.to(device)
-        self.crit_biases.to(device)
-
-        # Belief networks
-        self.b_net_weights.to(device)
-        self.b_net_biases.to(device)
-
-        # Encoder networks
-        self.e_net_weights.to(device)
-        self.e_net_biases.to(device)
-
-        # Filter networks
-        self.f_net_weights.to(device)
-        self.f_net_biases.to(device)
-
-        # Decoder networks
-        self.d_net_weights.to(device)
-        self.d_net_biases.to(device)
-
-        # Update networks (mean and bias)
-        self.u_mean_net_weights.to(device)
-        self.u_mean_net_biases.to(device)
-        self.u_bias_net_weights.to(device)
-        self.u_bias_net_biases.to(device)
-        
-
-    def forward(self, lv):
-        """
-        Outputs the heterogeneous weights using the latent variable
-        """
-
-        return TensorDict({
-            "policy": self.get_weights(lv, self.p_net_weights, self.p_net_biases, self.config.d_action),
-            "q1": self.get_weights(lv, self.crit_weights, self.crit_biases, 1), 
-            "q2": self.get_weights(lv, self.crit_weights, self.crit_biases, 1),
-            "belief": self.get_weights(lv, self.b_net_weights, self.b_net_biases, self.config.d_beliefs),
-            "encoder": self.get_weights(lv, self.e_net_weights, self.e_net_biases, self.config.d_comm_state),
-            "filter": self.get_weights(lv, self.f_net_weights, self.f_net_biases, self.config.d_message), 
-            "decoder": self.get_weights(lv, self.d_net_weights, self.d_net_biases, self.config.d_comm_state), 
-            "update_mean": self.get_weights(lv, self.u_mean_net_weights, self.u_mean_net_biases, self.config.d_relation),
-            'update_std': self.get_weights(lv, self.u_bias_net_weights, self.u_bias_net_biases, self.config.d_relation),
-        }, device = self.config.device)
-    
-    def get_weights(self, lv, weight_net, bias_net, dims):
-        w = weight_net(lv)
-        w = w.reshape((-1, dims, self.config.d_het_weights))
-        b = bias_net(lv)
-
-        # w = torch.nn.functional.layer_norm(w, [self.config.d_het_weights])
-        # b = torch.nn.functional.layer_norm(b, [dims])
-        w = w.cpu() * self.config.hypernet_scale_factor
-        b = b.cpu() * self.config.hypernet_scale_factor
-
-        return TensorDict({
-            "weight": w, 
-            "bias": b,
-        })
     
 class PPOLatentDecoder(nn.Module): 
     def __init__(self, config: ParameterSettings):
@@ -241,9 +139,7 @@ class HyperNetwork (nn.Module):
     def __init__(self, config : ParameterSettings):
         super().__init__()
         self.latent_encoder = LatentEncoder(config)
-        if config.type == "sac":
-            self.latent_decoder = SACLatentDecoder(config)
-        elif config.type == "ppo":
+        if config.type == "ppo":
             self.latent_decoder = PPOLatentDecoder(config)
 
     def to(self, device):
