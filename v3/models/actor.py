44a5c8f965155d9c05e44d63ed8ed99dbfe78cdb
joaquin <62501229+TheRealAnonymous008@users.noreply.github.com>
2025-06-17 11:03:48 +0800

    Update actor.py

--- v3/models/actor.py
+++ v3/models/actor.py
@@ -111,7 +111,7 @@ class DecoderUpdate(nn.Module):
         sigma = self.update_cov_net.apply_heterogeneous_weights(self.update_cov_net(input), us_weights)
 
         eps = torch.randn_like(sigma)
-        newMij = mu
+        newMij = mu + eps * sigma
 
         zdj = normalize_vector(zdj)
         newMij = normalize_vector(newMij)
